4c7522938ff3e967dc69c3b54a05bb9e5edbed61
wu.yanqi <wu.yanqi@eisoo.com>
2019-02-28 16:31:05 +0800

    auto_ptr，boost等聪明的指针

---
+++ execauto_ptr/execauto_ptr/execauto_ptr.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Double
+{
+public:
+	Double(double d = 0) : dValue(d) { cout << "constructor: " << dValue << endl; }
+	~Double() { cout << "destructor: " << dValue << endl; }
+	void setDouble(double d) { dValue = d; }
+private:
+	double dValue;
+};
+
+int main()
+{
+
+}
+
+void main2()
+{
+	// 此时不会执行dou的析构函数，会造成内存泄漏
+	// 需要显示的执行delete dou
+	Double *dou = new Double(9.0);
+
+	// delete dou;
+}
+
+void main1()
+{
+	// auto_ptr会强制执行class的析构函数
+	auto_ptr<Double> ptr(new Double(3.14));
+	(*ptr).setDouble(6.28);
+}
