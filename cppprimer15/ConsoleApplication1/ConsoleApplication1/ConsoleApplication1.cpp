be3db98cf9ea8f8421c1638bd16aed6385ac8290
wu.yanqi <wu.yanqi@eisoo.com>
2019-05-16 17:35:28 +0800

    c++，OOP，动态绑定

---
+++ cppprimer15/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include <iostream>
+
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+private:
+	static int i;
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
