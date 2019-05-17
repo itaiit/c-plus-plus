74b99bb5a11d8989f885a5834f25e8568c3421f9
wu.yanqi <wu.yanqi@eisoo.com>
2019-05-17 17:58:41 +0800

    C++：OOP补充

--- cppprimer15/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ cppprimer15/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,25 +1,62 @@
 ﻿#include "pch.h"
 #include <iostream>
+using namespace std;
 
-class MyClass
-{
+class BaseClass {
 public:
-	MyClass();
-	~MyClass();
+	virtual void func() {
 
+	}
+	virtual void func(int i) {
+		
+	}
+	virtual void func(int i, int j) {
+
+	}
+	void func(string str) {
+
+	}
 private:
-	static int i;
+	int age;
 };
 
-MyClass::MyClass()
-{
-}
+class DeriveClass : public BaseClass {
+public:
+	using BaseClass::func;
 
-MyClass::~MyClass()
-{
-}
+	virtual void func() override {
+
+	}
+	virtual void func(int i, int j) override {
+		this->age = i; // this指向的对象不能被改变
+	}
+	int getAge() {
+		static int num = 0;
+		return age;
+	}
+private:
+	int age;
+};
+
+class CopyClass {
+public:
+	CopyClass() = default;
+	CopyClass(CopyClass & cla) = delete;
+};
+
+//CopyClass::CopyClass(CopyClass & cla) {
+//	cout << "调用拷贝构造函数" << endl;
+//}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	/*DeriveClass *dc = new DeriveClass;
+	dc->func();
+
+	CopyClass c1, c2(c1);*/
+	// c1 = c2;
+	DeriveClass dcl1;
+	dcl1.func(100, 300);
+	cout << dcl1.getAge() << endl;
+	
 }
