c42b88e7b4845d716afff71697206f409081c644
wu.yanqi <wu.yanqi@eisoo.com>
2019-05-21 17:59:39 +0800

    c++：map，pair

---
+++  temporary/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	/*
+		1. map
+		2. 使用pair初始化map
+		3. pair的public成员first和second
+	*/
+	map<string, int> mp;
+	pair<string, int> pp("pairKey", 100);
+	mp.insert(pp);
+
+	cout << mp["pairKey"] << endl;
+	
+	cout << mp.at("pairKey") << endl;
+
+	cout << "pair.first:" << pp.first << endl;
+	cout << "pair.second:" << pp.second << endl;
+
+}
